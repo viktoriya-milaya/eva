1eb39eeb155ad8571c6c413a95ba8023804f7062
Viktoriya <vika.milaya92@gmail.com>
2023-05-23 06:40:49 +0300

    add slider

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,16 @@ import getApiData from './api';
 import showMobileMenu from './mobile';
 import showForm  from './modal';
 import createProductsList from './products';
+import showSlider from './slider';
 import sendRequest from './form';
 
 document.addEventListener('DOMContentLoaded', () => {
 
   window.navigator?.geolocation.getCurrentPosition(({ coords }) => getApiData(coords));
+  
   showMobileMenu();
   showForm ();
   createProductsList();
+  showSlider();
   sendRequest();
   });
